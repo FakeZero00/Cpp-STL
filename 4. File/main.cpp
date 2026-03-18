c3530a0bb0411df85cbd2562672c8edd3ca71f2f
FakeZero <sjlimdaa@naver.com>
2026-03-18 17:20:57 +0900

    03.18

--- 4. File/main.cpp
+++ 4. File/main.cpp
@@ -4,10 +4,13 @@
 #include <Windows.h>
 #include <random>
 #include <fstream>
+#include <string>
 #include <algorithm>
+#include <ranges>
 #include <array>
 #include <numeric>
 #include <print>
+#include <filesystem>	//C++17
 #include "save.h"
 using namespace std;
 
@@ -15,6 +18,37 @@ using namespace std;
 random_device rd;
 default_random_engine dre{ rd() };
 uniform_int_distribution<int> uid{ numeric_limits<int>::min(), numeric_limits<int>::max() };
+uniform_int_distribution<int> uidNum{ 1, 9999 };
+uniform_int_distribution<int> uidNameLen{ 10, 30 };
+uniform_int_distribution<int> uidChar{ 'a', 'z' };
+
+class Dog
+{
+public:
+	Dog() : num{ uidNum(dre) }
+	{
+		size_t len = uidNameLen(dre);
+		for (int i = 0; i < len; i++) {
+			name += uidChar(dre);
+		}
+	}
+	/*
+	int getNum() const
+	{
+		return num;
+	}
+	*/
+	
+private:
+	string name;		//[10, 30]글자 사이의 랜덤한 이름, 소문자로만
+	int num;			//[1, 9999] == [1, 10000)
+
+	friend ostream& operator<<(ostream& os, const Dog& dog)
+	{
+		print(os, "[{:3}] - {}", dog.num, dog.name);
+		return os;
+	}
+};
 
 int main()
 {
@@ -112,6 +146,7 @@ int main()
 		out.write(reinterpret_cast<char*>(&i), sizeof(int));
 	*/
 
+	/*
 	//[문제] int 1000개를 바이너리로 저장하고("int천개.bin"), int천개를 메모리로 읽어와라.
 	ofstream out{ "int천개.bin", ios::binary };
 
@@ -123,7 +158,57 @@ int main()
 	cout << endl;
 
 	out.write(reinterpret_cast<char*>(&a[0]), sizeof(int) * a.size());
+	*/
+	
 	//[문제] int천개.bin 파일이 있을 때, 해당 파일의 int값을 메모리로 읽어올 것
+	//int를 메모리 그대로 sizeof(int) 바이트만큼 저장하였다. 때문에 파일크기도 그만큼 저장되어 있어서 파일의 크기를 자료형의 개수로 볼 수도 있다.
+	//cout << "저장된 int 개수 " << filesystem::file_size("int천개.bin") / sizeof(int) << endl;
+
+	//1. 1000개수는 프로그램이 실행되는 동안 변하지 않는다.
+	//2. 개수가 감소는 가능해도 증가할 수는 없어야
+
+	//contiguous memory 공간상의 연속된 메모리 공간에 저장되는 자료구조. vector, array, string 등이 있다. list는 아니다.
+	/*
+	array<int, 1000> a;
+	ifstream in{ "int천개.bin", ios::binary };
+	if (not in)
+	{
+		cout << "파일을 열 수 없습니다." << endl;
+		system("pause");
+		return(20260317);
+	}
+
+	in.read((char*)(a.data()), sizeof(int) * a.size());
+	for (int num : a)
+		cout << num << '\t';
+	cout << endl;
+	*/
+	
+	//[예상문제] 이름 길이 순으로 늘여놓기, 이름 문자열에 a 안들어간 것만 출력하기
+	//읽기만 하는 데이터에 const넣는 거 잊지 말기...
+
+	//array <Dog, 1000> dogs;
+	
+	/*
+	sort(dogs.begin(), dogs.end(), [](const Dog& a, const Dog& b) {
+		return a.getNum() < b.getNum();
+		});
+	for (const Dog dog : dogs)
+		cout << dog << endl;
+	*/
+	/*
+	ranges::sort(dogs, {}, &Dog::getNum);
+	for (const Dog dog : dogs)
+		cout << dog << endl;
+	*/
+	
+	//파일 "Dog 천마리"를 바이너리 모드로 열어 dogs를 메모리 그대로 write 하였다.
+	//이때 Dog는 string(32바이트), int(4바이트)로 총 36바이트가 되지만 패딩(남은 메모리 공간)으로 인해 Dog 하나당 40바이트가 된다.
+	array <Dog, 1000> dogs;
+	ofstream out{ "Dog 천마리.bin", ios::binary };
+	out.write((char*)dogs.data(), dogs.size() * sizeof(Dog));
+	
+
 
 	save("main.cpp");
 	system("pause");
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <print>
+#include <array>
+#include <random>
 using namespace std;
+
+//[문제] 바이너리 모드로 저장한 파일 "Dog 천마리.bin"에는 아래와 같은 맴버를 갖는 Dog 객체
+//1000개가 저장되어 있다.
+//각 Dog 객체는 파일의 write 함수를 사용하여 sizeof(Dog) 바이트를 기록하였다.
+//파일 크기는 40'000 바이트이다.
+//메모리에 Dog 객체를 모두 읽어라.
+//화면에 출력하라.
+
+random_device rd;
+default_random_engine dre{ rd() };
+uniform_int_distribution<int> uid{ numeric_limits<int>::min(), numeric_limits<int>::max() };
+uniform_int_distribution<int> uidNum{ 1, 9999 };
+uniform_int_distribution<int> uidNameLen{ 10, 30 };
+uniform_int_distribution<int> uidChar{ 'a', 'z' };
+
+class Dog
+{
+public:
+	Dog() : num{ uidNum(dre) }
+	{
+		size_t len = uidNameLen(dre);
+		for (int i = 0; i < len; i++) {
+			name += uidChar(dre);
+		}
+	}
+	/*
+	int getNum() const
+	{
+		return num;
+	}
+	*/
+
+private:
+	string name;		//[10, 30]글자 사이의 랜덤한 이름, 소문자로만
+	int num;			//[1, 9999] == [1, 10000)
+
+	friend ostream& operator<<(ostream& os, const Dog& dog)
+	{
+		print(os, "[{:3}] - {}", dog.num, dog.name);
+		return os;
+	}
+};
 
 int main(void)
 {
-	cout << "안녕하세요, 월드!" << endl;
+	ifstream in{"Dog 천마리.bin", ios::binary};
+	if(not in){ 
+		cout << "파일을 열 수 없습니다." << endl;
+		system("pause");
+		return 404;
+	}
+
+	array<Dog, 1000> dogs;
+	in.read((char*)dogs.data(), sizeof(Dog) * dogs.size());
+
+	for(const Dog& dog : dogs)
+		cout << dog << endl;
 
 	system("pause");
 }
