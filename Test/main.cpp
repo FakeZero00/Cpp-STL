b4c6f5a1d5ab31e3c310a47c630ad2b7483eeb9c
FakeZero <sjlimdaa@naver.com>
2026-03-24 11:06:08 +0900

    동적할당 시작

--- Test/main.cpp
+++ Test/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <print>
 #include <array>
-#include <random>
+//#include <random>
 using namespace std;
 
 //[문제] 바이너리 모드로 저장한 파일 "Dog 천마리.bin"에는 아래와 같은 맴버를 갖는 Dog 객체
@@ -12,17 +12,22 @@ using namespace std;
 //파일 크기는 40'000 바이트이다.
 //메모리에 Dog 객체를 모두 읽어라.
 //화면에 출력하라.
+//->스트링 자체가 문제가 있었ㅇ므로 제대로된 문제가 아니었음
 
+/*
 random_device rd;
 default_random_engine dre{ rd() };
 uniform_int_distribution<int> uid{ numeric_limits<int>::min(), numeric_limits<int>::max() };
 uniform_int_distribution<int> uidNum{ 1, 9999 };
 uniform_int_distribution<int> uidNameLen{ 10, 30 };
 uniform_int_distribution<int> uidChar{ 'a', 'z' };
+*/
 
 class Dog
 {
-public:
+
+	/*
+	public:
 	Dog() : num{ uidNum(dre) }
 	{
 		size_t len = uidNameLen(dre);
@@ -30,48 +35,39 @@ public:
 			name += uidChar(dre);
 		}
 	}
-	/*
-	int getNum() const
-	{
-		return num;
-	}
 	*/
-
 private:
-	string name;		//[10, 30]글자 사이의 랜덤한 이름, 소문자로만
+	string name;		//[10, 30]글자 사이의 랜덤한 이름, 소문자로만. 주의: strirng은 문자열이 길면 스택에 저장되는게 아니라 free store에 저장된다. 짧으면 스택에 저장된다.
 	int num;			//[1, 9999] == [1, 10000)
 
 	friend ostream& operator<<(ostream& os, const Dog& dog)
 	{
-		print(os, "[{:3}] - {}", dog.num, dog.name);
+		print(os, "[{:4}] - {}", dog.num, dog.name);
 		return os;
 	}
 };
 
 int main(void)
 {
-	ifstream in{"Dog 천마리.bin", ios::binary};
+	array<Dog, 1000> dogs;
+	ifstream in{"Dog 천마리", ios::binary};
 	if(not in){ 
 		cout << "파일을 열 수 없습니다." << endl;
 		system("pause");
 		return 404;
 	}
 
-	array<Dog, 1000> dogs;
 	in.read((char*)dogs.data(), sizeof(Dog) * dogs.size());
-	cout << dogs[0] << endl;
 
-	/*
 	for(const Dog& dog : dogs)
 		cout << dog << endl;
-	*/
+	
+	
 	/*
 	array <Dog, 1000> dogs;
-	ofstream out{ "Dog 천마리.bin", ios::binary };
+	ofstream out{ "Dog 천마리", ios::binary };
 	out.write((char*)dogs.data(), dogs.size() * sizeof(Dog));
 	*/
-	
-	
 
 	system("pause");
 }
