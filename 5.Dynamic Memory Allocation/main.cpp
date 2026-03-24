ab734a35d6a9b720b45513fdabd444b1437e1fe6
FakeZero <sjlimdaa@naver.com>
2026-03-24 12:15:38 +0900

    추가

--- 5.Dynamic Memory Allocation/main.cpp
+++ 5.Dynamic Memory Allocation/main.cpp
@@ -1,5 +1,8 @@
 //Dynamic Memory Allocation
 #include <iostream>
+#include <array>
+#include <numeric>
+#include <memory>
 #include "Save.h"
 using namespace std;
 
@@ -16,7 +19,38 @@ int main()
 	//메모리의 내용을 1부터 시작하는 정수로 채워라.
 	//합계를 계산해서 화면에 출력하라.
 
-	
+	while (true) {
+		size_t size;
+		cout << "필요한 정수의 개수를 입력하시오: ";
+		cin >> size;
+
+		//int* ptr; C++에서는 포인터를 잘 안쓴다!
+
+		unique_ptr<int[]> ptr;	//RAII
+		try {
+			ptr.reset(new int[size]);	// new로 메모리를 할당받음. unique_ptr이 알아서 해제해줌.
+		}
+		catch (exception& e)
+		{
+			cout << e.what() << endl;
+			exit(404);
+		}
+
+		iota(ptr.get(), ptr.get() + size, 1);	// 1부터 시작하는 정수로 채우기
+
+		/*
+		long long sum{};
+		for (int i = 0; i < size; i++)
+			sum += ptr[i];
+		*/
+
+		long long sum = accumulate(ptr.get(), ptr.get() + size, 0LL);	//3번째 인자는 초기값. 초기값도 잘 자료형을 정해줘야 함. 0LL은 long long 타입의 0을 의미.
+
+		cout << "1부터 " << size << "까지의 합계: " << sum << endl;
+
+		//delete[] ptr;	//new, 포인터를 써서 데이터를 할당 받았을 때, delte 안하면 할당 해제가 되지 않음. 램 누수가 일어나서 점점 느려짐.
+						//심지어 delete가 2번 발동될 경우 프로그램이 완전히 죽어버림.
+	}
 
 	save("main.cpp");
 	system("pause");
