f19857d19dc2071027820bcf34d8b6fe8fb77726
FakeZero <sjlimdaa@naver.com>
2026-03-31 12:18:00 +0900

    Callable Type

--- 5.Dynamic Memory Allocation/main.cpp
+++ 5.Dynamic Memory Allocation/main.cpp
@@ -10,6 +10,7 @@
 #include <array>
 #include <print>
 #include <ranges>
+#include <chrono>
 #include "Save.h"
 using namespace std;
 
@@ -20,6 +21,13 @@ using namespace std;
 //스마트 포인터를 통해 할당과 해제를 자동으로 할 수 있다.
 
 //Callable Type
+//C++11에서 람다식 처음 도입
+//이름없는 함수 람다 - 가장 간단한 형태
+//왜 이름이 없을까? - 부를 필요가 없기 때문.
+//호출 가능한 타입
+//1. 함수
+//2. lambda
+//3. 함수호출연산자를 오버로딩한 클래스 - 이 클래스로 만든 객체는 호출 가능하다.
 
 //================================================================
 
@@ -57,6 +65,25 @@ int compare(const void* a, const void* b)
 	return *(int*)a - *(int*)b;
 }
 
+bool stdCompare(int a, int b)
+{
+	return a > b;
+}
+
+int f()
+{
+	cout << "지금 호출된 것은 그냥 함수입니다." << endl;
+	return 0;
+}
+
+class Dog
+{
+public:
+	void operator()() {
+		cout << "왈왈" << endl;
+	}
+};
+
 int main()
 {
 	/*
@@ -93,7 +120,7 @@ int main()
 
 		delete[] ptr;	//new, 포인터를 써서 데이터를 할당 받았을 때, delte 안하면 할당 해제가 되지 않음. 램 누수가 일어나서 점점 느려짐.
 					//심지어 delete가 2번 발동될 경우 프로그램이 완전히 죽어버림.
-}
+	}
 	*/
 	//Dog* p = new Dog;
 	//RAII로 자원관리의 어려움을 해결할 수 있다.
@@ -123,16 +150,75 @@ int main()
 	
 	//[문제] 랜덤 값을 갖는 int 1000만개를 메모리에 저장하라.
 	//오름차순으로 정렬하고 화면에 출력하라.
-	for (int& num : a)
-		num = uid(dre);
+	//for (int& num : a)
+	//	num = uid(dre);
 
-	qsort(a.data(), a.size(), sizeof(array<int, 1000'0000>::value_type), compare);
+	//{	//qsort
+	//	// 걸린 시간 측정
+	//	//시작
+	//	auto start = chrono::high_resolution_clock::now();
+	//	qsort(a.data(), a.size(), sizeof(int), compare);
+	//	auto stop = chrono::high_resolution_clock::now();
+	//	//auto duration = stop - start;
+	//	auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+	//	cout << "qsort 걸린 시간: " << duration << endl;
+	//	//끝
+	//	//정렬이진 화면 출력
+	//	/*for (int num : a | views::take(1000))
+	//		print("{:8}", num);
 
-	//정렬이진 화면 출력
-	for (int num : a | views::reverse | views::take(1000))
-		print("{:8}", num);
+	//	cout << endl;*/
+	//}
+	//
+	//for (int& num : a)
+	//	num = uid(dre);
 
-	cout << endl;
+	//{	//std::sort
+	//	// 걸린 시간 측정
+	//	//시작
+	//	auto start = chrono::high_resolution_clock::now();
+	//	//함수를 람다로 전달하면 람다식 자체가 하나의 함수의 탬플릿 객체가 되어서 sort함수의 코드 안에 집어넣어 버리는데 이러면 일반 함수 호출처럼
+	//	//함수가 있는 주소번지로 왔다 갔다하는 오버헤드가 없어지게 되서 성능이 좋아지는데, 이를 함수의 인라인화라고 한다.
+	//	sort(a.begin(), a.end(), [](int a, int b) {
+	//		return a > b;
+	//		});
+	//	auto stop = chrono::high_resolution_clock::now();
+	//	//auto duration = stop - start;
+	//	auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+	//	cout << "std::sort 걸린 시간: " << duration << endl;
+	//	//끝
+	//	//정렬이진 화면 출력
+	//	for (int num : a | views::take(1000))
+	//		print("{:8}", num);
+
+	//	cout << endl;
+	//}
+
+	//만든 람다식을 호출하려면 바로 뒤에 ()를 붙여주면 된다.
+	/*cout << "람다식 타입: " << typeid([]() {
+		cout << "Hello, Lambda!" << endl;
+		}).name() << endl;*/
+
+	//람다식의 클래스는 다음과 같은 구조로 되어 있다.
+	//class HelloLambda {
+	//public:
+	//	void operator()() {
+	//		cout << "Hello, Lambda!" << endl;
+	//	}
+	//};
+
+	//HelloLambda a;
+	//a();
+	////위에서 만든 클래스 타입과 람다식의 타입이 비슷한 것을 알 수 있다.
+	//cout << "람다식 타입: " << typeid(a).name() << endl;
+
+	//함수도 호출 가능한 타입이므로 함수 포인터로도 호출할 수 있다.
+	/*int (*a)(void) = f;
+	a();
+	cout << "람다식 타입: " << typeid(f).name() << endl;*/
+
+	Dog dog;
+	dog();
 
 	save("main.cpp");
 	system("pause");
